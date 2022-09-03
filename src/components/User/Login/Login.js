daa5ec53fa7cb95b4c85b265509003cab1650df8
LazyTurtle-chiu <76042466+LazyTurtle-chiu@users.noreply.github.com>
2022-09-04 00:12:47 +0800

    create login page with css

--- src/components/User/Login/Login.js
+++ src/components/User/Login/Login.js
@@ -1,15 +1,37 @@
+import React from "react";
 import { Link } from "react-router-dom";
+
+import "./login.css";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to="/register">
-        <button>Register Page</button>
-      </Link>
-      <Link to="/profile">
-        <button>Profile Page</button>
-      </Link>
+    <div className="login">
+      <h1 className="heading">LOGIN</h1>
+      <p className="sub-heading">Please enter your e-mail and password:</p>
+      <form>
+        <div className="bundle">
+          <div>
+            <label htmlFor="email">
+              <h3 className="label">Email</h3>
+            </label>
+          </div>
+          <input type="text" id="email" />
+        </div>
+        <div className="bundle">
+          <div>
+            <label htmlFor="password">
+              <h3 className="label">Password</h3>
+            </label>
+          </div>
+          <input type="text" id="password" />
+        </div>
+        <button className="submit-btn">LOGIN</button>
+      </form>
+      <div>
+        <p>
+          Don't have an account? <Link to="/register">Create One</Link>
+        </p>
+      </div>
     </div>
   );
 };
