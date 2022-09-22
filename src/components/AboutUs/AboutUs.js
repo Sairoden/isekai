26e075ad49d2c52b9c5372ddd24e1a10d1841df7
Sairoden <odingandarosa@gmail.com>
2022-09-22 16:48:05 +0800

    Change about us texts

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -10,25 +10,31 @@ const AboutUs = () => {
         <div className="heading">About Us</div>
         <div className="sub-heading text-center">
           <h2>
-            "Velit soluta facere natus ducimus, animi exercitationem autem atque
-            delectus fugiat mollitia aperiam, iusto at necessitatibus ipsam
-            deserunt blanditiis in voluptas vel minima."
+            Isekai is a retail experience created for fans of all things
+            Japanese lifestyle and culture. We carry exclusive officially
+            licensed apparel, accessories, and more.
           </h2>
         </div>
         <div className="body">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam
-            dicta temporibus aliquid fugiat rem at vero, magnam illo blanditiis,
-            commodi dolore molestias esse. Saepe possimus, nisi nam maxime
-            dolore labore! Velit soluta facere natus ducimus, animi
-            exercitationem autem atque delectus fugiat mollitia aperiam, iusto
-            at necessitatibus ipsam deserunt blanditiis in voluptas vel minima.
-            Sequi eius, sunt consectetur soluta expedita quod. Quasi aperiam
-            veniam, voluptatem amet fugit illum numquam laboriosam labore optio
-            ex incidunt! Rerum blanditiis aliquid quia, eum numquam impedit aut
-            commodi. Nobis incidunt natus, nostrum ullam dolorem saepe
-            consequuntur!
-          </p>
+          <h3>
+            Isekai is a lifestyle brand inspired by Japanese streetwear and
+            anime culture. We began to make t-shirts for ourselves. After
+            discovering that our friends liked the designs, it was decided to
+            take it to the next level and build our own brand. This is how
+            isekai was founded in 2022 when we released our very first designs
+            at a small convention in the Philippines. Since then, isekai has
+            become a lifestyle brand that has collaborated with Hatsune Miku,
+            Doki Doki Literature Club, Kizuna AI and more. One of the reasons
+            that isekai is growing internationally is because we attend many
+            Asian pop culture and comic conventions throughout the year. Not
+            only do we attend conventions in The Philippines, but we also travel
+            to Singapore, Malaysia, Indonesia and Thailand. Once a year, we
+            visit Japan for inspiration and meetings. In addition, isekai has
+            different retailers located all over Asia, which also creates local
+            communities. Something that once started with an edgy anime t-shirt
+            from enthusiastic teenagers, has now become a growing international
+            brand and community.
+          </h3>
         </div>
       </div>
       <Footer />
