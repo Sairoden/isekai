792ba6263e8177790e02497569c7ceceb793dd0b
LazyTurtle-chiu <76042466+LazyTurtle-chiu@users.noreply.github.com>
2022-09-29 16:39:57 +0800

    updated the css

--- src/components/Products/AirpodCaseProductPage.js
+++ src/components/Products/AirpodCaseProductPage.js
@@ -14,6 +14,7 @@ import "../Nav/Nav.css";
 import "../Shop/Shop.css";
 
 import { CartContext } from "../Context/CartContext";
+import React from "react";
 
 const initialState = techAccessories;
 
@@ -31,7 +32,7 @@ const AirpodCaseProductPage = () => {
 
   useEffect(() => {
     setFilteredProduct(
-      data.find(item => {
+      data.find((item) => {
         return item.id == id;
       })
     );
@@ -77,8 +78,8 @@ const AirpodCaseProductPage = () => {
         <p>Customers who brought this item also brought</p>
 
         <div className="container">
-          {randomProduct.map(item => {
-            const productUrl = item => {
+          {randomProduct.map((item) => {
+            const productUrl = (item) => {
               if (item.category === "apparel") {
                 if (item.keywords.includes("Vaporwave")) {
                   return "vaporwave-aesthetic-clothing-tees-hoodies-merch";
@@ -138,8 +139,8 @@ const AirpodCaseProductPage = () => {
         <p>Customers who viewed this item also viewed</p>
 
         <div className="container">
-          {randomApparel.map(item => {
-            const productUrl = item => {
+          {randomApparel.map((item) => {
+            const productUrl = (item) => {
               if (item.category === "apparel") {
                 if (item.keywords.includes("Vaporwave")) {
                   return "vaporwave-aesthetic-clothing-tees-hoodies-merch";
